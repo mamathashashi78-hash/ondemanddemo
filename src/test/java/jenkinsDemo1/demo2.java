0d97a90594568f9cf0113d808afed1016e00d8f8
Mamatha <mamathashashi78@gmail.com>
2025-08-12 12:07:16 +0530

    file added

---
+++ src/test/java/jenkinsDemo1/demo2.java
@@ -0,0 +1,10 @@
+package jenkinsDemo1;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello I'm mamatha");
+	}
+
+}
